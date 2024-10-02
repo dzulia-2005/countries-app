9a8379462ab15e0d036f4c42799683e9ca168e37
dzulia-2005 <159149786+dzulia-2005@users.noreply.github.com>
2024-10-03 00:36:06 +0400

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import '@/App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import  { Suspense, lazy } from 'react';
+import '@/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
 
 const LazyCardSectionview = lazy(() => import('@/pages/home/view/list/index'));
 const LazyAboutdescript = lazy(() => import('@/pages/about/component/description/index'));
@@ -10,15 +10,35 @@ const LazyContactInformation = lazy(() => import('@/pages/contact/component/info
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route element={<LazyDefaultlayout />}>
-            <Route path="/" element={<LazyCardSectionview />} />
-            <Route path="/about" element={<LazyAboutdescript />} />
-            <Route path="/contact" element={<LazyContactInformation />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route element={<LazyDefaultlayout />}>
+          {/* Wrapping each route in its own Suspense */}
+          <Route 
+            path="/" 
+            element={
+              <Suspense fallback={<div>Loading Home...</div>}>
+                <LazyCardSectionview />
+              </Suspense>
+            } 
+          />
+          <Route 
+            path="/about" 
+            element={
+              <Suspense fallback={<div>Loading About...</div>}>
+                <LazyAboutdescript />
+              </Suspense>
+            } 
+          />
+          <Route 
+            path="/contact" 
+            element={
+              <Suspense fallback={<div>Loading Contact...</div>}>
+                <LazyContactInformation />
+              </Suspense>
+            } 
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
